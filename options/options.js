c8231e4cef98e122b4ff1f9f9a8a20627785e14f
Crowdbotics <team@crowdbotics.com>
2021-08-16 21:46:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "winter_block_29861Identifier",
+  url: "https://winter_block_29861Identifier.botics.co",
+  api: "https://winter_block_29861Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
